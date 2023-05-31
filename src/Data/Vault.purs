239ca4a85a1881c357d0dc6a293768e854267075
Julien Chandra <mediapengunjungblog@gmail.com>
2023-05-31 23:54:25 +0700

    make it work in purs 0.15.9

--- src/Data/Vault.purs
+++ src/Data/Vault.purs
@@ -11,13 +11,12 @@ module Data.Vault
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Unsafe (unsafePerformEff)
-import Control.Monad.Eff.Ref (REF, Ref, newRef, writeRef, modifyRef')
-
 import Data.Function.Uncurried as Fn
 import Data.Maybe (Maybe(..))
 import Data.Vault.Internal as V
+import Effect (Effect)
+import Effect.Ref as Ref
+import Effect.Unsafe (unsafePerformEffect)
 
 -- | A persistent store for values of arbitrary types.
 newtype Vault = Vault (V.UniqueMap Item)
@@ -25,36 +24,36 @@ newtype Vault = Vault (V.UniqueMap Item)
 -- | Key for an item in vault
 data Key a = Key V.Unique (Item' a)
 
-type Item = Eff (ref :: REF) Unit
+type Item = Effect Unit
 
-type Item' a = Ref (Maybe a)
+type Item' a = Ref.Ref (Maybe a)
 
 -- | The empty vault
 empty :: Vault
 empty = Vault V.empty
 
 -- | Create a new key for use with a vault.
-newKey :: forall eff a. Eff (ref :: REF | eff) (Key a)
+newKey :: forall a. Effect (Key a)
 newKey = do
   k <- V.newUnique
-  ref <- newRef Nothing
+  ref <- Ref.new Nothing
   pure $ Key k ref
 
 -- | Insert a value for a given key. Overwrites any previous value.
 insert :: forall a. Key a -> a -> Vault -> Vault
-insert (Key k ref) a (Vault m) = Vault (Fn.runFn3 V.insert k (writeRef ref (Just a)) m)
+insert (Key k ref) a (Vault m) = Vault (Fn.runFn3 V.insert k (Ref.write (Just a) ref) m)
 
 -- | Delete a key from the vault.
 delete :: forall a. Key a -> Vault -> Vault
-delete (Key k _) (Vault m) = Vault (Fn.runFn2 V.delete k m)
+delete (Key k _) (Vault m) = Vault (Fn.runFn2 V.deleteUnique k m)
 
 -- | Lookup the value of a key in the vault.
 lookup :: forall a. Key a -> Vault -> Maybe a
 lookup (Key k ref) (Vault m) = case Fn.runFn4 V.lookup Nothing Just k m of
   Nothing  -> Nothing
-  Just act -> unsafePerformEff do
+  Just act -> unsafePerformEffect do
     _ <- act
-    modifyRef' ref rollItem
+    Ref.modify' rollItem ref
 
 union :: Vault -> Vault -> Vault
 union (Vault m) (Vault n) = Vault (Fn.runFn2 V.union m n)
